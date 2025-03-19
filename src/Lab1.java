703bf03f496a39e202800e089aa6d83eb63db9b2
Oleksii Aleshchenko <oleksii.aleshchenko@playtomax.com>
2025-03-19 09:52:21 +0200

    upd lab 1

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,56 +1,23 @@
 public class Lab1 {
     public static void main(String[] args) {
-        final char A = 1;
+        final char A = 3;
         final char N = 5;
-        final char B = 1;
+        final char B = 0;
         final char M = 5;
         final char C = 2;
-//        for(;;);
 
         double s = 0;
-        boolean isDivisionByZero = false;
-//        try {
-        label1: for (char i = A; i <= N && !isDivisionByZero; i++) {
-//            todo improve
-            /*if (i == C) {
-//                    continue;
-//                s = Double.POSITIVE_INFINITY;
-//                s = Math.MAX_VALUE;
-                isDivisionByZero = true;
-                break;
-            }*/
 
+        if ((C >= A && C <= N) || (0 >= B && 0 <= M)) {
+            System.out.println("Division by zero");
+            return;
+        }
+
+        for (char i = A; i <= N; i++) {
             for (char j = B; j <= M; j++) {
-                if (i == C) {
-                    isDivisionByZero = true;
-                    break /*label1*/;
-                    /*System.out.println("Division by zero");
-                    return;*/
-                }
-//                todo add check for j == 0
                 s += (double) (i % j) / (i - C);
             }
         }
-
-        if (isDivisionByZero) {
-            System.out.println("Division by zero");
-        } else {
-            System.out.println(s);
-        }
-        /*} catch (ArithmeticException e) {
-            System.out.println("Division by zero");
-        }*/
-
-//        System.out.println(-1.0 / 0);
-//        System.out.println(/*(double)*/ 0 / 0);
-
-
-//todo check in C/C++ and C#
-        /*System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1f / 2);*/
-
-        /*System.out.println(args[0]);
-        System.out.println(args[1]);*/
+        System.out.println(s);
     }
 }
